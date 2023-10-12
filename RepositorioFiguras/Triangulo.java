609cd532214cda4ba953ecfaabc8a34d19f16543
Panchito027 <jlopezb021@alumno.uaemex.mx>
2023-10-12 10:31:41 -0600

    ya

--- RepositorioFiguras/Triangulo.java
+++ RepositorioFiguras/Triangulo.java
@@ -1,14 +1,15 @@
 
-public class Triangulo implements Figura{
+public class Triangulo implements Figura {
     private double base;
     private double altura;
 
-    public Triangulo(double base,double altura){
+    public Triangulo(double base, double altura) {
         this.base = base;
         this.altura = altura;
     }
+
     @Override
     public double calcularArea() {
-        return (base * altura)/2;
+        return (base * altura) / 2;
     }
 }
