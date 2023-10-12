f40201bf4cf77537b46cb2dc475aefc26cb1e705
DanielColucci <142242668+DanielColucci@users.noreply.github.com>
2023-10-12 09:55:49 -0600

    Subo la clase figura, circulo y main

---
+++ RepositorioFiguras/Main.java
@@ -0,0 +1,9 @@
+package RepositorioFiguras;
+
+public class Main {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(5);
+
+        System.out.println("Área del círculo: " + circulo.calcularArea());
+    }
+}
